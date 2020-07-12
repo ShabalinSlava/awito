01a4bca6eec1c149c386abd0d43ecc7191051714
Shabalin Slava <svaznoy0309@gmail.com>
2020-07-12 20:47:10 +0800

    added card and esc close event

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -4,23 +4,45 @@ document.addEventListener('DOMContentLoaded', function(){
 	const addAd = document.querySelector('.add__ad')
 	const modalBtnSubmit = document.querySelector('.modal__btn-submit')
 	const modalSubmit = document.querySelector('.modal__submit')
+	const catalog = document.querySelector('.catalog')
+	const modalItem = document.querySelector('.modal__item')
+
+	// Функция закрытия модальных окон
+	const closeModal = function(event) {
+		const target = event.target;
+		if (target.classList.contains('modal__close') || target === this) {
+			this.classList.add('hide')
+			if (this === modalAdd) {
+				modalSubmit.reset(); // Очищение формы при закрытии и при новом открытии	
+			}	
+		}
+	};
+
+	// Функция закрытия модального окна через ESC
+	const closeModalEsc = function(event) {
+		if (event.code === 'Escape') {
+			modalAdd.classList.add('hide');
+			modalItem.classList.add('hide');
+			document.removeEventListener('keydown', closeModalEsc) // Событие удаляется когда закрываются модальные окна
+		}
+	}
 
 	// События
 	// Открытие модального окна
 	addAd.addEventListener('click', () => {
 		modalAdd.classList.remove('hide');
-		// Блокировака кноки отправить в модальном окне
-		modalBtnSubmit.disabled = true;
+		modalBtnSubmit.disabled = true; // Блокировака кноки отправить в модальном окне
+		document.addEventListener('keydown', closeModalEsc) // Событие закрытия модальных окон через ESC
 	});
-	// Закрытие модального окна с помощью делегирования
-	modalAdd.addEventListener('click', event => {
+	
+	modalAdd.addEventListener('click', closeModal); // Закрытие модального окна через функции closeModal
+	modalItem.addEventListener('click', closeModal); // Закрытие модального окна через функции closeModal
+	// Открываем карточку с товарами
+	catalog.addEventListener('click', event => {
 		const target = event.target;
-		if (target.classList.contains('modal__close') || 
-			target === modalAdd
-			) {
-			modalAdd.classList.add('hide')
-			// Очищение формы при закрытии и при новом открытии
-			modalSubmit.reset();
-		}
+		if (target.closest('.card')) {
+			modalItem.classList.remove('hide')
+			document.addEventListener('keydown', closeModalEsc) // Событие закрытия модальных окон через ESC
+		}	
 	});
 });
