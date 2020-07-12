2223ddd4ad3beb44730a4efe6c282a60e81d9339
Shabalin Slava <svaznoy0309@gmail.com>
2020-07-12 16:49:30 +0800

    added event opening modal window

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
-	alert('hallo')
-})
+	// Переменные
+	const modalAdd = document.querySelector('.modal__add')
+	const addAd = document.querySelector('.add__ad')
+	const modalBtnSubmit = document.querySelector('.modal__btn-submit')
+	const modalSubmit = document.querySelector('.modal__submit')
+
+	// События
+	// Открытие модального окна
+	addAd.addEventListener('click', () => {
+		modalAdd.classList.remove('hide');
+		// Блокировака кноки отправить в модальном окне
+		modalBtnSubmit.disabled = true;
+	});
+	// Закрытие модального окна с помощью делегирования
+	modalAdd.addEventListener('click', event => {
+		const target = event.target;
+		if (target.classList.contains('modal__close') || 
+			target === modalAdd
+			) {
+			modalAdd.classList.add('hide')
+			// Очищение формы при закрытии и при новом открытии
+			modalSubmit.reset();
+		}
+	});
+});
